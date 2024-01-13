b16caa9758b06b0b29cd0fccb350db5d55ea661f
Eden-Ha <edenmikael12@gmail.com>
2024-01-13 17:13:43 +0300

    some updates

--- src/axios.js
+++ src/axios.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const instance = axios.create({
   // THE API (cloud function) URL
-  baseURL: "http://127.0.0.1:10000",
-  // baseURL: "https://sa-amazon-backend.cyclic.app",
+  // baseURL: "http://127.0.0.1:10000",
+  baseURL: "https://sa-amazon-backend.cyclic.app",
 });
 
 // const instance = axios.create({
